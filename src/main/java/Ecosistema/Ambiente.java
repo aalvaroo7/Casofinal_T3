4184bedd807472fb6248c5f4642d79cc91f403bc
aalvaroo7 <almarrom@myuax.com>
2024-03-13 17:13:53 +0100

    comit

---
+++ src/main/java/Ecosistema/Ambiente.java
@@ -0,0 +1,15 @@
+
+package Ecosistema;
+class Ambiente {
+    String clima;
+    String terreno;
+    int recursosDisponibles;
+
+    Ambiente(String clima, String terreno, int recursosDisponibles) {
+        this.clima = clima;
+        this.terreno = terreno;
+        this.recursosDisponibles = recursosDisponibles;
+    }
+
+    // Métodos para interactuar con el ambiente
+}
